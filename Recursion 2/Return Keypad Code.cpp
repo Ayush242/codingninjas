0deb5e506125decd168ce77571f6068de31dfa1e
Ayush Mittal <45912958+Ayush242@users.noreply.github.com>
2023-07-26 19:07:50 +0530

    Update Return Keypad Code.cpp

--- Recursion 2/Return Keypad Code.cpp
+++ Recursion 2/Return Keypad Code.cpp
@@ -21,3 +21,34 @@
 // cd
 // ce
 // cf
+
+#include <string>
+using namespace std;
+string chars(int num){
+	string arr[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+	return arr[num];
+}
+
+int keypad(int  input,string output[]){
+
+	if(input == 0){
+		output[0] ="";
+		return 1;
+	}
+
+	int outSize = keypad(input/10, output);
+	int ans = input%10;
+	int k=0;
+	string str = chars(ans);
+	string arr[outSize];
+	for(int i=0;i<outSize;i++){
+		arr[i] =output[i];
+	}
+	for(int i=0;i<str.length();i++){
+		for(int j=0;j<outSize;j++){
+			output[k++] = arr[j] + str[i] ;
+		}
+	} 
+	return k;
+
+}

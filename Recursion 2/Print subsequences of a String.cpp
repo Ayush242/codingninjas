01850ad5e01f94653298ec80289ffb11595e1e7c
Ayush Mittal <45912958+Ayush242@users.noreply.github.com>
2023-08-06 19:20:11 +0530

    Update Print subsequences of a String.cpp

--- Recursion 2/Print subsequences of a String.cpp
+++ Recursion 2/Print subsequences of a String.cpp
@@ -1,1 +1,29 @@
+//  Given a string s, print all its subsequences
 
+
+//Approach 1
+#include<bits/stdc++.h>
+using namespace std;
+
+int subseq(string input, string output[]){
+  if(input.empty()){
+    output[0] ="";
+    return 1;
+  }
+  int len = subseq(input.substr(1), output);
+  for(int i=0;i<len;i++){
+    output[i+len] = input[0] + output[i];
+  }
+  return 2*len;
+}
+
+int main(){
+  string s;
+  cin>>s;
+  int len = pow(2,s.size());
+  string *output = new string[len];
+  int n = subseq(s,output);
+  for(int i=0;i<n;i++){
+    cout<<output[i]<<endl;
+  }
+}
